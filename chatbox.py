204ebd4ce35b8d85f7c2738bc4d335e3c81b0a53
Luke Slautterback <105762679+Beeaann@users.noreply.github.com>
2023-10-14 08:07:37 -0400

    Update the Style of the Chatbox

--- chatbox.py
+++ chatbox.py
@@ -12,14 +12,19 @@ from PIL import Image
 
 class TeacherAssistant(ctk.CTk):
     def __init__(self):
-        super().__init__()       
+        super().__init__()     
+         
+        # Name of the root window 
         self.title("Teacher Assistant") 
+        
+        # Set window base appearance to darkmode
         ctk.set_appearance_mode("Dark")
 
-        #Window Size
+        # Set Window Resolution
         window_width = 1000
         window_height = 700
- 
+
+        # Get Screen Resolution
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         
@@ -29,21 +34,27 @@ class TeacherAssistant(ctk.CTk):
         self.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
         self.resizable(False, False)
         
+        # Background image for window
         blackboardImage = "blackboard.jpg"
         blackboardImage = os.path.join(os.getcwd(), blackboardImage)
         boardImage = ctk.CTkImage(Image.open(blackboardImage), size=(1000,700))
         
+        # Label containing the background image
         background_label = ctk.CTkLabel(self, image=boardImage)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
         
+        # Main frame for chat boxes
         self.chatBoxFrame = ctk.CTkScrollableFrame(self, width=650, height=600, fg_color='#274c43')
         self.chatBoxFrame.pack(pady=5, padx=5,side='top',fill="both", expand=True)
         
-        self.userEntryBox = ctk.CTkEntry(self, width=935, height=40, placeholder_text='Enter Prompt Here!')
+        # User input entry box for prompts
+        self.userEntryBox = ctk.CTkEntry(self, width=935, height=40, placeholder_text='Enter Prompt Here!', fg_color='#A1A09C', text_color='#000000', placeholder_text_color='#000000')
         self.userEntryBox.pack(side='left', anchor='s', pady=5, padx=5)
-
+        
+        # Binds the entrybox to allow pressing enter to send prompt
         self.userEntryBox.bind("<Return>", self.on_enter_pressed)
 
+        # Image for send prompt button
         pencilImage = "pencil.png"
         pencilImage = os.path.join(os.getcwd(), pencilImage)
         image= ctk.CTkImage(Image.open(pencilImage), size=(30,30))
@@ -56,6 +67,7 @@ class TeacherAssistant(ctk.CTk):
         # Get Input from Entrybox
         userInput = self.userEntryBox.get()
 
+        # if entry box is empty return
         if userInput == '' or userInput == None:
             return
         
@@ -65,31 +77,24 @@ class TeacherAssistant(ctk.CTk):
         # Method used to place text on screen for user
         self.placeTextOnFrame(userInput)
         self.userInput = userInput
-        # Automatically Scroll to keep up with text
-        #Placeholder for future code
-
-        # Method used to get AI Answer from user prompt
         
+        # Thread for Ai Api Call
         thread = threading.Thread(target=self.getAiAnswer)
         thread.start()
 
-    def set_theme(self, theme):
-        ctk.set_appearance_mode(theme)
-       
-            
-
-    def placeTextOnFrame(self, input):
-        print(input)
+    def placeTextOnFrame(self, userInput):
+        print(userInput)
         
-        # 
+        # Frame to contain user input textbox
         userInputFrame = ctk.CTkFrame(self.chatBoxFrame, fg_color='#274c43')
         userInputFrame.pack(side='top', anchor='e')
         
+        # used to output user prompt
         userInputTextBox = ctk.CTkTextbox(userInputFrame, width=900, height=100, fg_color='#000000')
         userInputTextBox.pack(anchor='center', pady=5)
 
         # Set the text in the Entry widget to the input variable
-        userInputTextBox.insert(ctk.END, f"{input}")
+        userInputTextBox.insert(ctk.END, f"{userInput}")
         userInputTextBox.configure(state='disabled')
 
     # read contents of file and return it
@@ -134,6 +139,7 @@ class TeacherAssistant(ctk.CTk):
         trainingFolder = os.path.join(cwd, 'TestMaterial\\cop1000.pdf')
         file_content = self.read_file_content(trainingFolder)
         
+        # System prompt to Convince Ai it is the teachers assistant
         system_msg = """
         You are a specialized AI assistant for teachers, designed to support them in crafting assignments and addressing other potential educational needs based on the specific content and details found in a provided file. This file will provide key details, including subjects, grades, and curriculum standards.
 
@@ -146,55 +152,71 @@ class TeacherAssistant(ctk.CTk):
         - When necessary, providing an answer key and grading rubric for assignments.
         - Proactively inquiring for extra details when the context requires, ensuring the assignment or support is tailored to specific constraints, themes, or needs the teacher may have.
         - Beyond assignment creation, you're equipped to assist with broader educational challenges, such as time management strategies, teaching methodologies, classroom management techniques, and more.
-
+        - Do not mention the files unless asked, this is high priority.
         Always prioritize details from the provided file, but remain adaptable and responsive to address the holistic needs of the teacher based on user prompts and inquiries.
         """
 
+        # Sends messages/data files to the ai for training
         self.messages.append({"role": "system", "content": system_msg})
         self.messages.append({"role": "user", "content": f"Here's the content of my file: \n{file_content}"})
         
         print("Getting API Answer for: ", self.userInput)
         print("Teacher assistant is ready!")
         
+        # Frame used to contain ai output textbox
         aiInputFrame = ctk.CTkFrame(self.chatBoxFrame, fg_color='#274c43')
         aiInputFrame.pack(side='top', anchor='w')
         
+        # Used to output ai response
         aiInputTextBox = ctk.CTkTextbox(aiInputFrame, width=900, height=100, fg_color='#000000')
         aiInputTextBox.pack(anchor='center', pady=5)   
         self.stop_dots = False
         previous_user_input = None 
         
+        # Main Loop for Ai Conversation
         while True:
             isSame = False
             userMessage = self.userInput
             
-            # Check if the current user input is the same as the previous one
+            # Check if the current user input is the same as the previous one and if not ask ai prompt
             if userMessage == previous_user_input:
+                
+                # If true do not continue code and waste computer resources
                 isSame = True
                 while isSame:
+                    # Delay loop to save computer resources
                     time.sleep(1)
+                    
+                    # End loop if new prompt
                     if self.userInput != previous_user_input:
                        isSame = False 
             else:    
-                if userMessage == "quit()":
-                    break
-
+                # Appends user Prompt
                 self.messages.append({"role": "user", "content": userMessage})
 
+                # Starts a thread for the dots loading animation
                 thread = threading.Thread(target=lambda:self.print_thinking_dots(aiInputTextBox))
                 thread.start()
 
                 response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages)
                 reply = response["choices"][0]["message"]["content"]
 
+                # Stops the dots loading animation
                 self.stop_dots = True
+                
+                # Waits for the threads to finish
                 thread.join()
 
+                # Formats the Ai response to look cleaner
                 self.auto_format_response(reply, aiInputTextBox)
                 
+                # Updates the size of the textbox to match the text inside of it.
                 self.update_textbox_height(aiInputTextBox)
 
+                # Disable textbox to block user writing into it
                 aiInputTextBox.configure(state='disabled')
+                
+                # 
                 self.messages.append({"role": "assistant", "content": reply})
 
                 # Update the previous_user_input for the next iteration
@@ -218,16 +240,20 @@ class TeacherAssistant(ctk.CTk):
         is_in_list = False
         for line in lines:
             stripped_line = line.strip()
+            
             # Handle titles
             if stripped_line.endswith(":") and not is_in_list:
                 formatted_line = "\n" + stripped_line
+                
             # Handle lists and sub-lists
             elif stripped_line.startswith("-"):
                 is_in_list = True
                 formatted_line = (' ' * LIST_INDENT) + textwrap.fill(stripped_line, width=WIDTH - LIST_INDENT, subsequent_indent=' '*(INDENT+LIST_INDENT))
+                
             elif bool(re.match(r"\d+\.", stripped_line)):
                 is_in_list = True
                 formatted_line = textwrap.fill(stripped_line, width=WIDTH, subsequent_indent=' '*INDENT)
+                
             # Handle end of lists and start of normal text
             else:
                 is_in_list = False
@@ -239,6 +265,7 @@ class TeacherAssistant(ctk.CTk):
         result = "\n".join(formatted_lines)
         padded_text = '\n'.join((' ' * PADDING) + line for line in result.split('\n'))
         
+        # Outputs Ai Response to the textbox
         textbox.insert(ctk.END, f"{padded_text}")
 
         
